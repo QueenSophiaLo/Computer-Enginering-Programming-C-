023e6638e9a12822bfd2671148a92bd5af11a1f6
QueenSophiaLo <75154780+QueenSophiaLo@users.noreply.github.com>
2023-03-25 02:58:27 -0400

    Create Main.cpp

---
+++ C++ 2/Projects/Project 6: Doubly Linked List Class/Main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "doublylinkedlist.cpp"
+
+
+using namespace std;
+
+
+void test(){
+   DoublyLinkedList<int> dll;
+  
+   dll.push(1);
+   dll.push(2);
+   dll.push(3);
+  
+   DoublyLinkedList<int> dllCopy = dll;
+  
+   dllCopy.push(4);
+   dllCopy.remove(1);
+   dllCopy.remove(0);
+   dllCopy.insert(5, 1);
+  
+   dll.print();
+   dllCopy.print();
+  
+   dll = dllCopy;
+   dll.print();
+}
+
+
+int main(){
+   test();
+  
+   return 0;
+}
